0701d11565a5ff3079b34d11ea1bfb5efdcf8226
mattgonzalesced <m.gonzales@coolsys.com>
2024-12-12 09:23:15 -0600

    Update script.py

--- MyExtensions/MyFirstExtension.extension/mytools.tab/mytools.panel/testscript.pushbutton/script.py
+++ MyExtensions/MyFirstExtension.extension/mytools.tab/mytools.panel/testscript.pushbutton/script.py
@@ -3,7 +3,7 @@ from pyrevit import forms
 # Define the main function
 def main():
     # Create and show a simple message box
-    forms.alert("Goodbye!", title="pyRevit", warn_icon=False)
+    forms.alert("Hello there2!", title="pyRevit", warn_icon=False)
 
 # Run the main function
 if __name__ == "__main__":
