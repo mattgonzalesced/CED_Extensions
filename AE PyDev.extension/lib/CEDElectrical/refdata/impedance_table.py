6251f8e06c1c39aeadae312c7fcc3505bb0301f4
AEvelina129 <AntEvelina@gmail.com>
2025-05-02 08:30:20 -0400

    AE Dev - lots of garbage.

---
+++ AE PyDev.extension/lib/CEDElectrical/refdata/impedance_table.py
@@ -0,0 +1,142 @@
+WIRE_IMPEDANCE_TABLE = {
+    '12': {
+        'XL': {'PVC': 0.054, 'Steel': 0.068},
+        'R': {
+            'Copper': {'PVC': 2.00, 'Steel': 2.00},
+            'Aluminum': {'PVC': 3.20, 'Steel': 3.20}
+        }
+    },
+    '10': {
+        'XL': {'PVC': 0.050, 'Steel': 0.063},
+        'R': {
+            'Copper': {'PVC': 1.20, 'Steel': 1.20},
+            'Aluminum': {'PVC': 2.00, 'Steel': 2.00}
+        }
+    },
+    '8': {
+        'XL': {'PVC': 0.052, 'Steel': 0.065},
+        'R': {
+            'Copper': {'PVC': 0.78, 'Steel': 0.78},
+            'Aluminum': {'PVC': 1.30, 'Steel': 1.30}
+        }
+    },
+    '6': {
+        'XL': {'PVC': 0.051, 'Steel': 0.064},
+        'R': {
+            'Copper': {'PVC': 0.49, 'Steel': 0.49},
+            'Aluminum': {'PVC': 0.81, 'Steel': 0.81}
+        }
+    },
+    '4': {
+        'XL': {'PVC': 0.048, 'Steel': 0.060},
+        'R': {
+            'Copper': {'PVC': 0.31, 'Steel': 0.31},
+            'Aluminum': {'PVC': 0.51, 'Steel': 0.51}
+        }
+    },
+    '3': {
+        'XL': {'PVC': 0.047, 'Steel': 0.059},
+        'R': {
+            'Copper': {'PVC': 0.25, 'Steel': 0.25},
+            'Aluminum': {'PVC': 0.40, 'Steel': 0.40}
+        }
+    },
+    '2': {
+        'XL': {'PVC': 0.045, 'Steel': 0.057},
+        'R': {
+            'Copper': {'PVC': 0.19, 'Steel': 0.20},
+            'Aluminum': {'PVC': 0.32, 'Steel': 0.32}
+        }
+    },
+    '1': {
+        'XL': {'PVC': 0.046, 'Steel': 0.057},
+        'R': {
+            'Copper': {'PVC': 0.15, 'Steel': 0.16},
+            'Aluminum': {'PVC': 0.25, 'Steel': 0.25}
+        }
+    },
+    '1/0': {
+        'XL': {'PVC': 0.044, 'Steel': 0.055},
+        'R': {
+            'Copper': {'PVC': 0.12, 'Steel': 0.12},
+            'Aluminum': {'PVC': 0.20, 'Steel': 0.20}
+        }
+    },
+    '2/0': {
+        'XL': {'PVC': 0.043, 'Steel': 0.054},
+        'R': {
+            'Copper': {'PVC': 0.10, 'Steel': 0.10},
+            'Aluminum': {'PVC': 0.16, 'Steel': 0.16}
+        }
+    },
+    '3/0': {
+        'XL': {'PVC': 0.042, 'Steel': 0.052},
+        'R': {
+            'Copper': {'PVC': 0.077, 'Steel': 0.079},
+            'Aluminum': {'PVC': 0.13, 'Steel': 0.13}
+        }
+    },
+    '4/0': {
+        'XL': {'PVC': 0.041, 'Steel': 0.051},
+        'R': {
+            'Copper': {'PVC': 0.062, 'Steel': 0.063},
+            'Aluminum': {'PVC': 0.10, 'Steel': 0.10}
+        }
+    },
+    '250': {
+        'XL': {'PVC': 0.041, 'Steel': 0.052},
+        'R': {
+            'Copper': {'PVC': 0.052, 'Steel': 0.054},
+            'Aluminum': {'PVC': 0.085, 'Steel': 0.086}
+        }
+    },
+    '300': {
+        'XL': {'PVC': 0.041, 'Steel': 0.051},
+        'R': {
+            'Copper': {'PVC': 0.044, 'Steel': 0.045},
+            'Aluminum': {'PVC': 0.071, 'Steel': 0.072}
+        }
+    },
+    '350': {
+        'XL': {'PVC': 0.040, 'Steel': 0.050},
+        'R': {
+            'Copper': {'PVC': 0.038, 'Steel': 0.039},
+            'Aluminum': {'PVC': 0.061, 'Steel': 0.063}
+        }
+    },
+    '400': {
+        'XL': {'PVC': 0.040, 'Steel': 0.049},
+        'R': {
+            'Copper': {'PVC': 0.033, 'Steel': 0.035},
+            'Aluminum': {'PVC': 0.054, 'Steel': 0.055}
+        }
+    },
+    '500': {
+        'XL': {'PVC': 0.039, 'Steel': 0.048},
+        'R': {
+            'Copper': {'PVC': 0.027, 'Steel': 0.029},
+            'Aluminum': {'PVC': 0.043, 'Steel': 0.045}
+        }
+    },
+    '600': {
+        'XL': {'PVC': 0.039, 'Steel': 0.048},
+        'R': {
+            'Copper': {'PVC': 0.023, 'Steel': 0.025},
+            'Aluminum': {'PVC': 0.036, 'Steel': 0.038}
+        }
+    },
+    '750': {
+        'XL': {'PVC': 0.038, 'Steel': 0.048},
+        'R': {
+            'Copper': {'PVC': 0.019, 'Steel': 0.021},
+            'Aluminum': {'PVC': 0.029, 'Steel': 0.031}
+        }
+    },
+    '1000': {
+        'XL': {'PVC': 0.037, 'Steel': 0.046},
+        'R': {
+            'Copper': {'PVC': 0.015, 'Steel': 0.018},
+            'Aluminum': {'PVC': 0.023, 'Steel': 0.025}
+        }
+    }
+}
