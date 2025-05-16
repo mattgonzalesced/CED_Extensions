05daadb5b150a4422fdaa92017d15e7eebd1b44f
mattgonzalesced <m.gonzales@coolsys.com>
2025-05-16 14:59:40 -0500

    Delete MG PyTools.extension directory

--- MG PyTools.extension/MG PyTools.tab/MG PyTools.panel/debug.pushbutton/script.py
+++
@@ -1,7 +0,0 @@
-def debug_element_parameters(element):
-    print("Parameters for Equipment ID {}:".format(element.Id.IntegerValue))
-    for p in element.Parameters:
-        name = p.Definition.Name
-        raw_val = p.AsString() if p.StorageType == DB.StorageType.String else None
-        disp_val = p.AsValueString()
-        print("  {}: raw='{}', display='{}'".format(name, raw_val, disp_val))
