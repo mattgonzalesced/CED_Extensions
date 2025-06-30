1c1bf834c214b97cc340bd1ef81aef7b8b259f02
AEvelina129 <AntEvelina@gmail.com>
2025-06-30 14:52:34 -0400

    fix(super-circuit): completely jacked this up
    destroyed this script to get it working with new prototype template project

--- AE PyDev.extension/AE PyDev.Tab/Test Buttons.Panel/Test2.stack/SuperCircuit.pushbutton/script.py
+++ AE PyDev.extension/AE PyDev.Tab/Test Buttons.Panel/Test2.stack/SuperCircuit.pushbutton/script.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 __title__ = "SUPER CIRCUIT"
 
-from pyrevit import DB, revit
-from pyrevit.revit.db import transaction, query
-from pyrevit import script
 import re
-from Snippets._elecutils import get_all_light_devices, get_all_panels, get_all_elec_fixtures, get_all_light_fixtures
-from System.Collections.Generic import List  # Import .NET List for IList compatibility
 
+from System.Collections.Generic import List  # Import .NET List for IList compatibility
+from pyrevit import DB, revit
+from pyrevit.revit.db import query
+
+from Snippets._elecutils import get_all_light_devices, get_all_panels, get_all_elec_fixtures, get_all_light_fixtures
 
 doc = revit.doc
 uidoc = revit.uidoc
@@ -68,16 +68,16 @@ def group_elements_by_circuit(elements, panel_elements):
         # Retrieve parameters using query functions
         panel_param = query.get_param(element, "CKT_Panel_CEDT")
         circuit_param = query.get_param(element, "CKT_Circuit Number_CEDT")
-        rating_param = query.get_param(element, "CKT_Rating_CED")
-        load_name_param = query.get_param(element, "CKT_Load Name_CEDT")
-        ckt_notes_param = query.get_param(element, "CKT_Schedule Notes_CEDT")
+        # rating_param = query.get_param(element, "CKT_Rating_CED")
+        # load_name_param = query.get_param(element, "CKT_Load Name_CEDT")
+        # ckt_notes_param = query.get_param(element, "CKT_Schedule Notes_CEDT")
 
         # Get actual parameter values
         panel_name = query.get_param_value(panel_param)
         circuit_number = query.get_param_value(circuit_param)
-        rating = query.get_param_value(rating_param)
-        load_name = query.get_param_value(load_name_param)
-        ckt_notes = query.get_param_value(ckt_notes_param)
+        # rating = query.get_param_value(rating_param)
+        # load_name = query.get_param_value(load_name_param)
+        # ckt_notes = query.get_param_value(ckt_notes_param)
 
         # Skip elements without valid `ckt-Panel` and `ckt-Circuit Number` entirely
         if not panel_name and not circuit_number:
@@ -91,10 +91,10 @@ def group_elements_by_circuit(elements, panel_elements):
                 "element_ids": [element.Id],
                 "panel_name": panel_name,
                 "panel_element": None,
-                "circuit_number": circuit_number,
-                "rating": rating,
-                "load_name": load_name,
-                "ckt_notes": ckt_notes
+                "circuit_number": circuit_number
+                # "rating": rating,
+                # "load_name": load_name,
+                # "ckt_notes": ckt_notes
             }
             unnamed_counter += 1
             continue
@@ -112,10 +112,10 @@ def group_elements_by_circuit(elements, panel_elements):
                 "element_ids": [],
                 "panel_name": panel_name,
                 "panel_element": panel_element,
-                "circuit_number": circuit_number,
-                "rating": rating,
-                "load_name": load_name,
-                "ckt_notes": ckt_notes
+                "circuit_number": circuit_number
+                # "rating": rating,
+                # "load_name": load_name,
+                # "ckt_notes": ckt_notes
             }
 
         # Ensure ElementId is collected
@@ -157,82 +157,82 @@ def main():
     system_type = DB.Electrical.ElectricalSystemType.PowerCircuit
 
     # Use a Transaction Group to handle multiple transactions together
-    tg = DB.TransactionGroup(doc, "Create and Update Circuits")
-    tg.Start()
+    # tg = DB.TransactionGroup(doc, "Create and Update Circuits")
+    # tg.Start()
 
-    try:
-        # First Transaction: Create circuits and assign to panels
-        created_systems = {}  # To store created systems and associate them with original groupings
 
-        with revit.Transaction("Create Circuits and Assign Panels"):
-            for key, data in grouped_elements:
-                print("Processing Panel: {} | Circuit: {}".format(key[0], key[1]))
-                sample_element = data['elements'][0]
-                voltage = query.get_param_value(query.get_param(sample_element, "Voltage_CED"))
-                poles = query.get_param_value(query.get_param(sample_element, "Number of Poles_CED"))
-                rating = query.get_param_value(query.get_param(sample_element, "CKT_Rating_CED"))
+    # First Transaction: Create circuits and assign to panels
+    created_systems = {}  # To store created systems and associate them with original groupings
 
-                # Debugging: Check for missing rating parameter
-                if rating is None:
-                    print("Skipping group {}: Missing or invalid rating parameter.".format(key))
-                    continue
+    with revit.Transaction("Create Circuits and Assign Panels"):
+        for key, data in grouped_elements:
+            print("Processing Panel: {} | Circuit: {}".format(key[0], key[1]))
+            sample_element = data['elements'][0]
+            voltage = query.get_param_value(query.get_param(sample_element, "Voltage_CED"))
+            poles = query.get_param_value(query.get_param(sample_element, "Number of Poles_CED"))
+            if voltage is None:
+                voltage = 1292
+            if poles is None:
+                poles = 1
+            # rating = query.get_param_value(query.get_param(sample_element, "CKT_Rating_CED"))
 
-                # Place placeholder
-                placeholder = add_placeholder_family(doc, voltage, poles)
-                doc.Regenerate()
-                data['element_ids'].append(placeholder.Id)
+            # Debugging: Check for missing rating parameter
+            # if rating is None:
+            #     print("Skipping group {}: Missing or invalid rating parameter.".format(key))
+            #     continue
 
-                if data["panel_element"] and data["element_ids"]:
-                    created_system = create_electrical_system(doc, data["element_ids"], system_type, data["panel_element"])
-                    if created_system:
-                        created_systems[created_system.Id] = key  # Link to the group key
-                    else:
-                        print("Skipped creating system for: {}".format(key))
+            # Place placeholder
+            placeholder = add_placeholder_family(doc, voltage, poles)
+            doc.Regenerate()
+            data['element_ids'].append(placeholder.Id)
 
-            for key, data in unnamed_elements.items():
-                if data["element_ids"]:
-                    created_system = create_electrical_system(doc, data["element_ids"], system_type, data["panel_element"])
-                    if created_system:
-                        created_systems[created_system.Id] = key
-                    else:
-                        print("Skipped creating system for unnamed group: {}".format(key))
+            if data["panel_element"] and data["element_ids"]:
+                created_system = create_electrical_system(doc, data["element_ids"], system_type, data["panel_element"])
+                if created_system:
+                    created_systems[created_system.Id] = key  # Link to the group key
+                else:
+                    print("Skipped creating system for: {}".format(key))
 
-        # Collect all electrical systems created in the project
-        all_systems = DB.FilteredElementCollector(doc).OfClass(DB.Electrical.ElectricalSystem).ToElements()
+        for key, data in unnamed_elements.items():
+            if data["element_ids"]:
+                created_system = create_electrical_system(doc, data["element_ids"], system_type, data["panel_element"])
+                if created_system:
+                    created_systems[created_system.Id] = key
+                else:
+                    print("Skipped creating system for unnamed group: {}".format(key))
 
-        # Second Transaction: Update circuit parameters based on original group data
-        with revit.Transaction("Update Circuit Parameters"):
-            for system in all_systems:
-                if system.Id in created_systems:
-                    key = created_systems[system.Id]
-                    # Check if it's a grouped element or unnamed
-                    if key in dict(grouped_elements):
-                        data = dict(grouped_elements)[key]
-                    else:
-                        data = unnamed_elements.get(key)
+    # Collect all electrical systems created in the project
+    # all_systems = DB.FilteredElementCollector(doc).OfClass(DB.Electrical.ElectricalSystem).ToElements()
 
-                    # Update parameters
-                    if data:
-                        # Example of updating circuit parameters: "ckt-Rating" and "ckt-Load Name"
-                        rating_param = system.get_Parameter(DB.BuiltInParameter.RBS_ELEC_CIRCUIT_RATING_PARAM)
-                        load_name_param = system.get_Parameter(DB.BuiltInParameter.RBS_ELEC_CIRCUIT_NAME)
-                        ckt_notes_param = system.get_Parameter(DB.BuiltInParameter.RBS_ELEC_CIRCUIT_NOTES_PARAM)
+    # Second Transaction: Update circuit parameters based on original group data
+    # with revit.Transaction("Update Circuit Parameters"):
+    #     for system in all_systems:
+    #         if system.Id in created_systems:
+    #             key = created_systems[system.Id]
+    #             # Check if it's a grouped element or unnamed
+    #             if key in dict(grouped_elements):
+    #                 data = dict(grouped_elements)[key]
+    #             else:
+    #                 data = unnamed_elements.get(key)
+    #
+    #             # Update parameters
+    #             if data:
+    #                 # Example of updating circuit parameters: "ckt-Rating" and "ckt-Load Name"
+    #                 rating_param = system.get_Parameter(DB.BuiltInParameter.RBS_ELEC_CIRCUIT_RATING_PARAM)
+    #                 load_name_param = system.get_Parameter(DB.BuiltInParameter.RBS_ELEC_CIRCUIT_NAME)
+    #                 ckt_notes_param = system.get_Parameter(DB.BuiltInParameter.RBS_ELEC_CIRCUIT_NOTES_PARAM)
+    #
+    #                 if rating_param and data["rating"]:
+    #                     rating_param.Set(data["rating"])
+    #
+    #                 if load_name_param and data["load_name"]:
+    #                     load_name_param.Set(data["load_name"])
+    #
+    #                 if ckt_notes_param and data["ckt_notes"]:
+    #                     ckt_notes_param.Set(data["ckt_notes"])
 
-                        if rating_param and data["rating"]:
-                            rating_param.Set(data["rating"])
-
-                        if load_name_param and data["load_name"]:
-                            load_name_param.Set(data["load_name"])
-
-                        if ckt_notes_param and data["ckt_notes"]:
-                            ckt_notes_param.Set(data["ckt_notes"])
-
-        # Commit the transaction group to save all changes
-        tg.Assimilate()
-
-    except Exception as e:
-        print("Error occurred: {}. Rolling back all changes.".format(e))
-        tg.RollBack()
+    # Commit the transaction group to save all changes
+    # tg.Assimilate()
 
 if __name__ == "__main__":
     main()
