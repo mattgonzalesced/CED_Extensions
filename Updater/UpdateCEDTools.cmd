98504acb8e15bb627075a09666d756840f3b395e
mattgonzalesced <m.gonzales@coolsys.com>
2025-07-29 16:50:45 -0500

    Got rid of old updater file

--- Updater/UpdateCEDTools.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-:: Runs the PowerShell update script in a new console window
-PowerShell -Command "Set-ExecutionPolicy Unrestricted -Scope Process" >> "%TEMP%\StartupLog.txt" 2>&1
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "C:\Users\Aevelina\CED_Extensions\Updater\UpdateCEDTools.ps1"
-@REM
-@REM @echo off
-@REM :: Launch PowerShell in a new console window and keep it open
-@REM start powershell.exe -NoExit -ExecutionPolicy Bypass -File "C:\Users\Aevelina\CED_Extensions\Updater\UpdateCEDTools.ps1"
