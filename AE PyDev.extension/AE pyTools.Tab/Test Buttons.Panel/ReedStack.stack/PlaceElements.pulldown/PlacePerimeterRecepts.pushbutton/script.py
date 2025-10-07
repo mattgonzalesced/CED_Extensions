b60a137857baa59072faea86191d87337b33b48d
reedpinch <reedpinch@gmail.com>
2025-10-07 12:46:34 -0400

    Made a basic organized library, deleted some unused buttons in ReedTools, and remade "place receptacles" using this structure.

--- AE PyDev.extension/AE pyTools.Tab/Test Buttons.Panel/ReedStack.stack/PlaceElements.pulldown/PlacePerimeterRecepts.pushbutton/script.py
+++ AE PyDev.extension/AE pyTools.Tab/Test Buttons.Panel/ReedStack.stack/PlaceElements.pulldown/PlacePerimeterRecepts.pushbutton/script.py
@@ -1,6 +1,6 @@
 # RP button: Place Perimeter Recepts.pushbutton/script.py
 from organized.MEPKit.revit.appdoc import get_doc
-from organized.MEPKit.core.logging import get_logger
+from organized.MEPKit.core.log import get_logger
 from organized.MEPKit.electrical.perimeter_runner import place_perimeter_recepts
 
 doc = get_doc()
