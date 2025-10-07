cb4c9838abcf3077247b8ba437d56b34ed8fc378
reedpinch <reedpinch@gmail.com>
2025-10-07 12:56:46 -0400

    Made a basic organized library, deleted some unused buttons in ReedTools, and remade "place receptacles" using this structure.

--- AE PyDev.extension/AE pyTools.Tab/Test Buttons.Panel/ReedStack.stack/PlaceElements.pulldown/PlacePerimeterRecepts.pushbutton/script.py
+++ AE PyDev.extension/AE pyTools.Tab/Test Buttons.Panel/ReedStack.stack/PlaceElements.pulldown/PlacePerimeterRecepts.pushbutton/script.py
@@ -1,3 +1,12 @@
+# -*- coding: utf-8 -*-
+import os, sys
+_here = os.path.dirname(__file__)
+while _here and not os.path.isdir(os.path.join(_here, 'lib')):
+    _here = os.path.dirname(_here)
+org = os.path.join(_here, 'lib', 'organized')
+if org not in sys.path:
+    sys.path.insert(0, org)
+
 # RP button: Place Perimeter Recepts.pushbutton/script.py
 from organized.MEPKit.revit.appdoc import get_doc
 from organized.MEPKit.core.log import get_logger
@@ -6,4 +15,5 @@ from organized.MEPKit.electrical.perimeter_runner import place_perimeter_recepts
 doc = get_doc()
 log = get_logger("PerimeterRecepts", level="INFO")
 count = place_perimeter_recepts(doc, logger=log)
-log.info("Done. Placed: {}".format(count))
+alert("Placed {} perimeter receptacle(s).\nCheck the pyRevit Output panel for details."
+      .format(count), title="Perimeter Recepts", warn=False)
--- AE PyDev.extension/lib/organized/MEPKit/core/log.py
+++ AE PyDev.extension/lib/organized/MEPKit/core/log.py
@@ -1,11 +1,26 @@
 # -*- coding: utf-8 -*-
 import logging
 
+# lib/organized/MEPKit/core/log.py
 def get_logger(name="MEPKit", level="INFO"):
-    log = logging.getLogger(name)
-    if not log.handlers:
-        h = logging.StreamHandler()
-        h.setFormatter(logging.Formatter('%(levelname)s: %(message)s'))
-        log.addHandler(h)
-    log.setLevel(getattr(logging, level.upper(), logging.INFO))
-    return log
+    try:
+        # Use pyRevit's output-aware logger (shows in Output panel)
+        from pyrevit import script as _script
+        return _script.get_logger()
+    except Exception:
+        import logging as _logging
+        log = _logging.getLogger(name)
+        if not log.handlers:
+            h = _logging.StreamHandler()
+            h.setFormatter(_logging.Formatter('%(levelname)s: %(message)s'))
+            log.addHandler(h)
+        log.setLevel(getattr(_logging, level.upper(), _logging.INFO))
+        return log
+
+def alert(msg, title="MEPKit", warn=False):
+    try:
+        from pyrevit import forms
+        forms.alert(msg, title=title, warn_icon=warn)
+    except Exception:
+        # Fallback: print if not in pyRevit context
+        print("[{}] {}".format(title, msg))
